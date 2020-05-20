25f8e4d2622549e3107a1f81ee13e90703630ecd
Chris Page <chris_pagey100@hotmail.com>
2020-05-20 11:37:36 +0100

    Completed Exercise 5 and moving on to the Week 1 Assignment

---
+++ Exercises/Asteroids/Assets/Scripts/Ship.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class handles the ships behaviour
+/// </summary>
+
+public class Ship : MonoBehaviour
+{
+
+    // Serialize fields
+
+    [SerializeField]
+    Sprite spriteShip;
+    [SerializeField]
+    Sprite spriteShipThrust;
+
+    // Fields to reduce clutter
+
+    private new Rigidbody2D rigidbody2D;
+    SpriteRenderer spriteRenderer;
+    Vector2 thrustDirection;
+
+    // Constants associated with movement
+
+    const float ThrustForce = 2;
+    const float RotateDegreesPerSecond = 90;
+    float collCircleRadius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ensure start position is 0,0 and load various components
+        Vector3 startPosition = new Vector3(0, 0, 10);
+        gameObject.transform.position = startPosition;
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        collCircleRadius = gameObject.GetComponent<CircleCollider2D>().radius;
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+        // Fixed update used to reduce fps load
+    {
+        // Handle rotation
+        float rotationInput = Input.GetAxis("Rotate");
+
+        if(rotationInput != 0)
+        {
+            float rotationAmount = RotateDegreesPerSecond * Time.deltaTime;
+            if (rotationInput < 0)
+            {
+                rotationAmount *= -1;
+            }
+            gameObject.transform.Rotate(Vector3.forward, rotationAmount);
+        }
+
+        // Store direction the ship is now facing following rotation
+
+        float currentRotationRadians = Mathf.Deg2Rad * gameObject.transform.eulerAngles.z;
+        thrustDirection = new Vector2(Mathf.Cos(currentRotationRadians), Mathf.Sin(currentRotationRadians));
+
+        // Handle ship thrusting
+
+        if (Input.GetAxis("Thrust") > 0)
+        {
+            rigidbody2D.AddForce(thrustDirection * ThrustForce, ForceMode2D.Force);
+            Vector3 currentPosition = gameObject.transform.position;
+            spriteRenderer.sprite = spriteShipThrust;
+        } else
+        {
+            spriteRenderer.sprite = spriteShip;
+        }
+
+    }
+
+    /// <summary>
+    /// Use OnBecameInvisible method for screen wrapping
+    /// </summary>
+    private void OnBecameInvisible()
+    {
+        Vector3 currentPosition = gameObject.transform.position;
+        Vector2 currentDirection = rigidbody2D.velocity;
+        print(currentDirection);
+
+        if (currentPosition.x < ScreenUtils.ScreenLeft)
+        {
+            currentPosition.x = ScreenUtils.ScreenRight + collCircleRadius;
+        } else if (currentPosition.x > ScreenUtils.ScreenRight)
+        {
+            currentPosition.x = ScreenUtils.ScreenLeft - collCircleRadius;
+        }
+        if (currentPosition.y < ScreenUtils.ScreenBottom)
+        {
+            currentPosition.y = ScreenUtils.ScreenTop + collCircleRadius;
+        }
+        else if (currentPosition.y > ScreenUtils.ScreenTop)
+        {
+            currentPosition.y = ScreenUtils.ScreenBottom - collCircleRadius;
+        }
+
+        gameObject.transform.position = currentPosition;
+    }
+
+}
---
+++ Exercises/Exercise 5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***********************");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("***********************");
+
+            Console.Write("\nWhat option would you like to choose? ");
+
+            int userSelection = int.Parse(Console.ReadLine());
+            while (userSelection != 4)
+            {
+                Console.WriteLine("Invalid entry. Please try again.\n");
+                Console.Write("\nWhat option would you like to choose? ");
+                userSelection = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
