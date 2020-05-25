c8176e8c8a237e4908ea87bfd77da355da8014c4
Chris Page <chris_pagey100@hotmail.com>
2020-05-25 13:31:16 +0100

    Exercise 15 completed

---
+++ Exercises/Exercise 15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Pyramid Slot Number (slot number, block letter, lit): ");
+            string input = Console.ReadLine();
+            string[] splitInput = input.Split(',');
+            int slotNumber = int.Parse(splitInput[0]);
+            char blockLetter = char.Parse(splitInput[1]);
+            bool lit = bool.Parse(splitInput[2]);
+            Console.WriteLine("Slot Number: " + slotNumber);
+            Console.WriteLine("Block Letter: " + blockLetter);
+            Console.WriteLine("Lit: " + lit);
+        }
+    }
+}
