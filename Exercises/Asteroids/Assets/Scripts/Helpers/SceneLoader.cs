4f4de42e4393059797209bc7926c3bc0c88603a0
Chris Page <chris_pagey100@hotmail.com>
2020-05-30 08:45:45 +0100

    Adding in menu

---
+++ Exercises/Asteroids/Assets/Scripts/Helpers/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Single);
+        }
+    }
+}
